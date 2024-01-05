092a01a69351c0a454826d947e7706c0c9b89262
villaneuvo <111202012812@mhs.dinus.ac.id>
2024-01-05 19:17:04 +0700

    [Update Streamlit and ipynb] Rename Title and some code in ipynb

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,12 +99,12 @@ df_final['target'] = y
 
 # STREAMLIT
 st.set_page_config(
-  page_title = "Hungarian Heart Disease",
+  page_title = "Hungarian Heart Disease Predictor",
   page_icon = ":heart:"
 )
 
 st.title("Hungarian Heart Disease")
-st.write(f"**_Model's Accuracy_** :  :green[**{accuracy}**]% (:red[_Do not copy outright_])")
+st.write(f"**_Model's Accuracy_** :  :green[**{accuracy}**]% (:red[Other accuracy may be different])")
 st.write("")
 
 tab1, tab2 = st.tabs(["Single-predict", "Multi-predict"])
